9e4831acba3802633a1f587bea802c90844f6dd8
unknown <evikody06@gmail.com>
2024-11-18 20:16:09 +0800

    Ask the user to input their personal information

---
+++ personal_info_collector/personal_info_collector.py
@@ -0,0 +1,6 @@
+# Ask the user for personal information
+name = input("Please enter you full name: ")
+age = input("Please enter your age: ")
+address = input("Please enter your address: ")
+number = input("Please enter your phone number: +63")
+email = input("Please enter your email address: ")
