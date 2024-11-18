cacdea7d9224e52c38659db53443112e32ac19f7
unknown <evikody06@gmail.com>
2024-11-18 20:22:23 +0800

    Displays the collected information

--- personal_info_collector/personal_info_collector.py
+++ personal_info_collector/personal_info_collector.py
@@ -4,3 +4,9 @@ age = input("Please enter your age: ")
 address = input("Please enter your address: ")
 number = input("Please enter your phone number: +63")
 email = input("Please enter your email address: ")
+
+# Variable for all the collected information
+personal_info = f"Full Name: {name}\nAge: {age}\nAddress: {address}\nNumber: {number}\nEmail Address: {email}"
+
+# Prints the personal information
+print(personal_info)
