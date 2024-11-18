27b91fc31f951a8ec04c23e285c781f8c9f4ab26
unknown <evikody06@gmail.com>
2024-11-18 20:28:53 +0800

    A loop for continuing the input

--- personal_info_collector/personal_info_collector.py
+++ personal_info_collector/personal_info_collector.py
@@ -1,12 +1,18 @@
-# Ask the user for personal information
-name = input("Please enter you full name: ")
-age = input("Please enter your age: ")
-address = input("Please enter your address: ")
-number = input("Please enter your phone number: +63")
-email = input("Please enter your email address: ")
+while True:
+    # Ask the user for personal information
+    name = input("Please enter you full name: ")
+    age = input("Please enter your age: ")
+    address = input("Please enter your address: ")
+    number = input("Please enter your phone number: +63")
+    email = input("Please enter your email address: ")
 
-# Variable for all the collected information
-personal_info = f"Full Name: {name}\nAge: {age}\nAddress: {address}\nNumber: {number}\nEmail Address: {email}"
+    # Variable for all the collected information
+    personal_info = f"Full Name: {name}\nAge: {age}\nAddress: {address}\nNumber: {number}\nEmail Address: {email}"
 
-# Prints the personal information
-print(personal_info)
+    retry = input("Do you want to enter another person's information? (Yes/No): ").lower()
+    if retry != "yes":
+        print("Exiting the program...")
+        break
+
+    # Prints the personal information
+    print(personal_info)
