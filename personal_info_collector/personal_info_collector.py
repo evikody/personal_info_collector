edca6a3c5ed2576ca4a9a77492e33cd372738b6d
unknown <evikody06@gmail.com>
2024-11-18 21:51:45 +0800

    A loop for asking a valid email address

--- personal_info_collector/personal_info_collector.py
+++ personal_info_collector/personal_info_collector.py
@@ -1,8 +1,8 @@
 # A loop that will continue asking until the user input no
 while True:
+
     # A loop that checks if the full name has at least 2 words
     while True:
-        # Ask the user for personal information
         name = input("Please enter you full name: ").strip()
         if len(name.split()) < 2:
             print("Invalid. Please enter both your first and last name.")
@@ -27,26 +27,30 @@ while True:
         number = input("Please enter your phone number: +63")
         if len(number) != 10:
             print("Invalid. Please enter a valid phone number.")
+        elif not number.isdigit():
+            print("Try Again. Only numbers are allowed.")
         else:
             break
 
     while True:
         email = input("Please enter your email address: ").strip()
-        if "@" and ".com" in email:
+        if "@" in email and ".com" in email:
             break
         else:
             print("Invalid. Please enter a valid email address")
 
     # Variable for all the collected information
-    personal_info = f"Full Name: {name}\nAge: {age}\nAddress: {address}\nNumber: {number}\nEmail Address: {email}"
+    personal_info = (f"Full Name: {name}\n"
+                     f"Age: {age}\n"
+                     f"Address: {address}\n"
+                     f"Number: {number}\n"
+                     f"Email Address: {email}\n"
+                     f"{'-' * 40}\n")
 
     # Ask the user if they want to input another person's information
     retry = input("Do you want to enter another person's information? (Yes/No): ").lower()
 
     # If the user input "no", then it the program will stop
     if retry != "yes":
-        print("Exiting the program...")
+        print("Thank you! Have a nice day!")
         break
-
-    # Prints the personal information
-    print(personal_info)
